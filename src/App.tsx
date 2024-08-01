f29838ca27f9f6800117fbc01c0d5424d9114dc4
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-08-02 00:45:43 +0200

    feat: :sparkles: implementing simple routing for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,64 @@
-import './App.css'
+import { useState } from "react";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from "../src/components/Home";
+import About from "../src/components/About";
+import Contact from "../src/components/Contact";
+import Blog from "../src/components/Blog";
+import Login from "../src/components/Login";
+import Signup from "../src/components/Signup";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  }
+  const logout = () => {
+    setIsAuthenticated(false);
+  }
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-    <h1>Freelancer marketplace</h1>
-  </div>
+    <div>
+      <nav>
+        <div>
+          <a href="/">SkillSpace</a>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/blog">Blog</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              {isAuthenticated ? (
+                <button onClick={logout}>Logout</button>
+              ) : (
+                <button onClick={login}>Login</button>
+              )}
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h2>This is the home page of my Skill Space application that helps people get jobs</h2>
+    </div>
+  )
+}
+
+export default Home;
