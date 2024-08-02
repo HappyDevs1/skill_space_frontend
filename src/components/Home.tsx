b6f2d3b54ce2e9cc05eaa95b89781503f2eb0e30
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-08-02 15:56:20 +0200

    feat: :sparkles: adding the location input

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,28 @@
 import { FaSearch } from "react-icons/fa";
+import { CiLocationOn } from "react-icons/ci";
+import Select from "react-select";
 
 function Home() {
+  const options = [
+    { value: 'option1', label: 'Option 1' },
+    { value: 'option2', label: 'Option 2' },
+    { value: 'option3', label: 'Option 3' },
+  ];
+
+  const customStyles = {
+    control: (provided: any) => ({
+      ...provided,
+      border: 'none',
+      boxShadow: 'none',
+      minHeight: '2.5rem',
+      height: '100%',
+    }),
+    placeholder: (provided: any) => ({
+      ...provided,
+      color: 'gray',
+      fontSize: '0.875rem', // Tailwind equivalent for text-sm
+    }),
+  };
   return (
     <div className="background flex">
       <div className="m-20">
@@ -10,7 +32,8 @@ function Home() {
         <p className="text-slate-600">Explore a diverse array of career opportunities in 
           <br /> multiple sectors. Whether you're seeking <br /> a position in technology, healthcare, finance, <br /> or any other industry, we have the perfect role <br /> to match your skills and aspirations.</p>
       </div>
-      <div className="flex items-center border border-gray-300 rounded mt-20 mt-auto">
+      <div className="flex items-center border border-gray-400 rounded mt-20 mt-auto p-5">
+      <div className="flex items-center rounded mt-20 mt-auto">
       <div className="flex items-center px-2">
         <FaSearch className="text-gray-500" />
       </div>
@@ -22,6 +45,33 @@ function Home() {
       <button className="bg-blue-500 text-white px-4 py-2 rounded-r hover:bg-blue-600">
         Search
       </button>
+    </div>
+    <div className="flex items-center rounded mt-20 mt-auto">
+      <div className="flex items-center px-2">
+        <CiLocationOn className="text-gray-500" />
+      </div>
+      <div className="w-48">
+      <Select
+          options={options}
+          placeholder="Location"
+          classNamePrefix="react-select"
+          styles={customStyles}
+        />
+      </div>
+    </div>
+    <div className="flex items-center rounded mt-20 mt-auto">
+      <div className="flex items-center px-2">
+        <CiLocationOn className="text-gray-500" />
+      </div>
+      <div className="w-48">
+      <Select
+          options={options}
+          placeholder="Location"
+          classNamePrefix="react-select"
+          styles={customStyles}
+        />
+      </div>
+    </div>
     </div>
     </div>
   )
