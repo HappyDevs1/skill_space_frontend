7f52dfa7a626a7f431640cb988fcb9b476c4db6d
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-08-02 19:45:18 +0200

    feat: :sparkles: finishing up styling the search component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -26,69 +26,75 @@ function Home() {
     }),
   };
   return (
-    <div className="background flex">
-      <div className="m-20 ">
-        <p className="font-bold text-sky-500">TOP CAREER OPPORTUNITIES</p>
-        <p className="text-4xl font-semibold mt-2 mb-5">Find your ideal job 
-          <br /> accross all industries</p>
-        <p className="text-slate-600">Explore a diverse array of career opportunities in 
-          <br /> multiple sectors. Whether you're seeking <br /> a position in technology, healthcare, finance, <br /> or any other industry, we have the perfect role <br /> to match your skills and aspirations.</p>
-      </div>
-      <div className="flex items-center border border-gray-400 rounded mt-auto p-5">
-      <div className="flex items-center rounded mt-20 mt-auto">
-      <div className="flex items-center px-2">
-        <FaSearch className="text-gray-500" />
-      </div>
-      <input
-        type="text"
-        placeholder="Search for jobs"
-        className="flex-grow py-2 px-2 focus:outline-none"
-      />
-      <button className="bg-blue-500 text-white px-4 py-2 rounded-r hover:bg-blue-600">
-        Search
-      </button>
-    </div>
-    <div className="flex items-center rounded mt-20 mt-auto">
-      <div className="flex items-center px-2">
-        <CiLocationOn className="text-gray-500" />
-      </div>
-      <div className="w-48">
-      <Select
-          options={options}
-          placeholder="Location"
-          classNamePrefix="react-select"
-          styles={customStyles}
+    <div className="flex flex-col items-center min-h-screen">
+    <div className="background w-full">
+  <div className="m-20 self-start">
+    <p className="font-bold text-sky-500">TOP CAREER OPPORTUNITIES</p>
+    <p className="text-4xl font-semibold mt-2 mb-5">
+      Find your ideal job <br /> across all industries
+    </p>
+    <p className="text-slate-600">
+      Explore a diverse array of career opportunities in <br /> multiple sectors. Whether you're seeking <br /> a position in technology, healthcare, finance, <br /> or any other industry, we have the perfect role <br /> to match your skills and aspirations.
+    </p>
+  </div>
+  <div className="flex flex-col items-center justify-center w-full flex-grow">
+    <div className="flex items-center border border-gray-400 rounded p-4 w-max">
+      <div className="flex items-center rounded border border-gray-300">
+        <div className="flex items-center p-2">
+          <FaSearch className="text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search for jobs"
+          className="flex-grow py-2 px-2 focus:outline-none"
         />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-r hover:bg-blue-600">
+          Search
+        </button>
+      </div>
+      <div className="flex items-center rounded ml-4 border border-gray-300">
+        <div className="flex items-center px-2">
+          <CiLocationOn className="text-gray-500" />
+        </div>
+        <div className="w-48">
+          <Select
+            options={options}
+            placeholder="Location"
+            classNamePrefix="react-select"
+            styles={customStyles}
+          />
+        </div>
+      </div>
+      <div className="flex items-center rounded ml-4 border border-gray-300">
+        <div className="flex items-center px-2">
+          <VscGraph className="text-gray-500" />
+        </div>
+        <div className="w-48">
+          <Select
+            options={options}
+            placeholder="Job level"
+            classNamePrefix="react-select"
+            styles={customStyles}
+          />
+        </div>
+      </div>
+      <div className="flex items-center rounded ml-400 border border-gray-300">
+        <div className="flex items-center px-2">
+          <IoBriefcaseOutline className="text-gray-500" />
+        </div>
+        <div className="w-48">
+          <Select
+            options={options}
+            placeholder="Department"
+            classNamePrefix="react-select"
+            styles={customStyles}
+          />
+        </div>
       </div>
     </div>
-    <div className="flex items-center rounded mt-20 mt-auto">
-      <div className="flex items-center px-2">
-      <VscGraph className="text-gray-500"/>
-      </div>
-      <div className="w-48">
-      <Select
-          options={options}
-          placeholder="Job level"
-          classNamePrefix="react-select"
-          styles={customStyles}
-        />
-      </div>
-    </div>
-    <div className="flex items-center rounded mt-20 mt-auto">
-      <div className="flex items-center px-2">
-      <IoBriefcaseOutline className="text-gray-500"/>
-      </div>
-      <div className="w-48">
-      <Select
-          options={options}
-          placeholder="Department"
-          classNamePrefix="react-select"
-          styles={customStyles}
-        />
-      </div>
-    </div>
-    </div>
-    </div>
+  </div>
+  </div>
+</div>
   )
 }
 
