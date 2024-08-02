2f1b46af8969f19c483dae11b044f1d15d2bf7fb
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-08-02 13:30:02 +0200

    feat: :sparkles: creating the home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,13 @@
 function Home() {
   return (
-    <div>
-      <h2>This is the home page of my Skill Space application that helps people get jobs</h2>
+    <div className="background flex">
+      <div className="m-20">
+        <p className="font-bold text-sky-500">TOP CAREER OPPORTUNITIES</p>
+        <p className="text-4xl font-semibold mt-2 mb-5">Find your ideal job 
+          <br /> accross all industries</p>
+        <p className="text-slate-600">Explore a diverse array of career opportunities in 
+          <br /> multiple sectors. Whether you're seeking <br /> a position in technology, healthcare, finance, <br /> or any other industry, we have the perfect role <br /> to match your skills and aspirations.</p>
+      </div>
     </div>
   )
 }
