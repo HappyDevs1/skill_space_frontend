5f8dd80886e1033379ea6cfe4a71a22c7b7628f2
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-08-02 15:18:40 +0200

    feat: :sparkles: implementing the search icon in the home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,5 @@
+import { FaSearch } from "react-icons/fa";
+
 function Home() {
   return (
     <div className="background flex">
@@ -8,6 +10,19 @@ function Home() {
         <p className="text-slate-600">Explore a diverse array of career opportunities in 
           <br /> multiple sectors. Whether you're seeking <br /> a position in technology, healthcare, finance, <br /> or any other industry, we have the perfect role <br /> to match your skills and aspirations.</p>
       </div>
+      <div className="flex items-center border border-gray-300 rounded mt-20 mt-auto">
+      <div className="flex items-center px-2">
+        <FaSearch className="text-gray-500" />
+      </div>
+      <input
+        type="text"
+        placeholder="Search for jobs"
+        className="flex-grow py-2 px-2 focus:outline-none"
+      />
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-r hover:bg-blue-600">
+        Search
+      </button>
+    </div>
     </div>
   )
 }
