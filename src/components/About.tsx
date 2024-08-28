6b834af75f85f9fd753d8dd072957970aee52746
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-08-28 13:45:40 +0200

    style: :lipstick: adding first row of values card

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,7 @@
+import { LiaLightbulb } from "react-icons/lia";
+import { FaRegStar } from "react-icons/fa6";
+import { SiTicktick } from "react-icons/si";
+
 function About() {
   return (
     <div>
@@ -14,7 +18,7 @@ function About() {
               Post a free job
             </button>
             <button className="border border-gray-300 rounded px-2 py-0.5">
-              Lean more
+              Learn more
             </button>
           </div>
         </div>
@@ -63,6 +67,56 @@ function About() {
                     MONTHLY VISITS
                   </p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Add horizontal line here */}
+      <div className="flex justify-center py-24">
+        <div className="flex flex-col items-center">
+          <p className="font-bold text-3xl mb-6">Our values</p>
+          <p className="text-gray-500 mb-10">
+            These are the values of the company. I will come back to edit this
+            text. <br /> This is just a temporary text to get me started but I
+            will come back soon.
+          </p>
+          <div className="container m-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-24 mx-auto justify-items-center">
+              <div className="flex flex-col p-4 w-full max-w-xs shadow-xl rounded">
+                <div className="flex justify-between items-start">
+                  <LiaLightbulb className="self-start" />
+                  <p className="font-bold text-center flex-1">Innovation</p>
+                </div>
+                <p className="text-gray-500 text-left mt-2">
+                  This is a test message about one of the values of the company;
+                  however, I will come back and edit this text. This is just a
+                  test message; I will come back to edit the text.
+                </p>
+              </div>
+
+              <div className="flex flex-col p-4 w-full max-w-xs shadow-xl rounded">
+                <div className="flex justify-between items-start">
+                  <FaRegStar className="self-start" />
+                  <p className="font-bold text-center flex-1">Innovation</p>
+                </div>
+                <p className="text-gray-500 text-left mt-2">
+                  This is a test message about one of the values of the company;
+                  however, I will come back and edit this text. This is just a
+                  test message; I will come back to edit the text.
+                </p>
+              </div>
+
+              <div className="flex flex-col p-4 w-full max-w-xs shadow-lg rounded">
+                <div className="flex justify-between items-start">
+                  <SiTicktick className="self-start" />
+                  <p className="font-bold text-center flex-1">Innovation</p>
+                </div>
+                <p className="text-gray-500 text-left mt-2">
+                  This is a test message about one of the values of the company;
+                  however, I will come back and edit this text. This is just a
+                  test message; I will come back to edit the text.
+                </p>
               </div>
             </div>
           </div>
