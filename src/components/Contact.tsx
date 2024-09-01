67292ff1502bd80e8887f3506056020f3f2a9773
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-09-01 18:24:00 +0200

    style: :lipstick: implementing the contact form with inputs and all the tags

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -49,11 +49,38 @@ function Contact() {
         <div className="container mx-auto p-4">
           <div className="grid grid-cols-1 md:grid-cols-1 gap-0">
             <div className="flex items-center rounded-md border-2 my-20">
-              <div className="bg-white w-full px-5 py-5">
-                <p>Hello</p>
+              <div className="flex flex-col gap-7 bg-white w-full px-5 py-5">
+                <div className="flex gap-5">
+                <div className="flex flex-col">
+                  <label className="mb-2">Name</label>
+                  <input className="border-2 rounded px-2" placeholder="What's your name?"/>
+                </div>
+                <div className="flex flex-col">
+                  <label className="mb-2">Email</label>
+                  <input className="border-2 rounded px-2" placeholder="What's your email?"/>
+                </div>
+              </div>
+
+              <div className="flex gap-5">
+                <div className="flex flex-col">
+                  <label className="mb-2">Phone</label>
+                  <input className="border-2 rounded px-2" placeholder="What's your phone?"/>
+                </div>
+                <div className="flex flex-col">
+                  <label className="mb-2">Subject</label>
+                  <input className="border-2 rounded px-2" placeholder="Select subject" />
+                </div>
+              </div>
+
+              <div className="flex flex-col">
+                <label className="mb-2">Leave us a message</label>
+                <textarea placeholder="Please write your message" className="border-2 w-full h-24 px-2"></textarea>
+              </div>
+              <button className="self-start bg-blue-500 text-white px-3 py-2 rounded text-sm font-bold">Submit Message</button>
+              
               </div>
               <div className="bg-gray-100 w-full px-5 py-28">
-                <p className="font-bold text-xl mb-1">Contact details</p>
+                <p className="font-bold text-2xl mb-2">Contact details</p>
                 <p className="text-gray-600">This is a test message, I will come back to this text to change it. I will change it later.</p>
                 <div className="flex flex-row items-center gap-3 mt-3">
                 <CiMail className="text-xl"/>
