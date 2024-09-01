d843c6598b45dc5e658252b00b0c2ed6f743a527
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-09-01 02:47:33 +0200

    style: :lipstick: contact details sections

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,10 @@
 import { MdHeadsetMic } from "react-icons/md";
 import { FaRegStar } from "react-icons/fa6";
 import { PiBuildings } from "react-icons/pi";
+import { CiMail } from "react-icons/ci";
+import { BsTelephone } from "react-icons/bs";
+import { TiSocialYoutube, TiSocialLinkedin, TiSocialTwitter, TiSocialFacebook } from "react-icons/ti";
+import { AiFillInstagram } from "react-icons/ai";
 
 function Contact() {
   return (
@@ -41,8 +45,35 @@ function Contact() {
             </div>
           </div>
         </div>
-        <div>
-          
+
+        <div className="container mx-auto p-4">
+          <div className="grid grid-cols-1 md:grid-cols-1 gap-0">
+            <div className="flex items-center rounded-md border-2 my-20">
+              <div className="bg-white w-full px-5 py-5">
+                <p>Hello</p>
+              </div>
+              <div className="bg-gray-100 w-full px-5 py-28">
+                <p className="font-bold text-xl mb-1">Contact details</p>
+                <p className="text-gray-600">This is a test message, I will come back to this text to change it. I will change it later.</p>
+                <div className="flex flex-row items-center gap-3 mt-3">
+                <CiMail className="text-xl"/>
+                <p className="font-bold">contact@skillspace.com</p>
+                </div>
+                <div className="flex flex-row items-center gap-3 mt-5">
+                <BsTelephone className="text-md"/>
+                  <p className="font-bold">(012) 453 - 7583</p>
+                </div>
+                <p className="font-bold mt-5">Follow us</p>
+                <div className="flex gap-3 items-center mt-4">
+                  <TiSocialFacebook className="text-2xl"/>
+                  <TiSocialTwitter className="text-2xl"/>
+                  <AiFillInstagram className="text-xl"/>
+                  <TiSocialLinkedin className="text-2xl"/>
+                  <TiSocialYoutube className="text-2xl"/>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
