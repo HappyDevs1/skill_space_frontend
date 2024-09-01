8c8903f5c43955b022abe52d7d2bd18508535ffa
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-09-01 19:50:55 +0200

    style: :lipstick: adding more style to the accordion

--- src/components/AccordionExpandIcon.tsx
+++ src/components/AccordionExpandIcon.tsx
@@ -3,18 +3,17 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export default function AccordionExpandIcon() {
   return (
     <div className='flex flex-col gap-5 my-14'>
-      <Accordion className='border'>
+      <Accordion className='border px-2 py-3'>
         <AccordionSummary
           expandIcon={<ArrowDownwardIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography><p className='font-bold text-lg'>How can I post a free job opening?</p></Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -24,13 +23,13 @@ export default function AccordionExpandIcon() {
         </AccordionDetails>
       </Accordion>
 
-      <Accordion className='border'>
+      <Accordion className='border px-2 py-3'>
         <AccordionSummary
           expandIcon={<ArrowDownwardIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography><p className='font-bold text-lg'>What is the cost to post a featured job?</p></Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -40,13 +39,13 @@ export default function AccordionExpandIcon() {
         </AccordionDetails>
       </Accordion>
 
-      <Accordion className='border'>
+      <Accordion className='border px-2 py-3'>
         <AccordionSummary
           expandIcon={<ArrowDownwardIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography><p className='font-bold text-lg'>Are there bulk discounts for many job openings?</p></Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -56,13 +55,13 @@ export default function AccordionExpandIcon() {
         </AccordionDetails>
       </Accordion>
 
-      <Accordion className='border'>
+      <Accordion className='border px-2 py-3'>
         <AccordionSummary
           expandIcon={<ArrowDownwardIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography><p className='font-bold text-lg'>Are there bulk discounts for many job openings?</p></Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -72,13 +71,13 @@ export default function AccordionExpandIcon() {
         </AccordionDetails>
       </Accordion>
 
-      <Accordion className='border'>
+      <Accordion className='border px-2 py-3'>
         <AccordionSummary
           expandIcon={<ArrowDownwardIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography><p className='font-bold text-lg'>Do you offer discounts for non-profits?</p></Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
