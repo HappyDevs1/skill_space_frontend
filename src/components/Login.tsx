a9cacc41b87c9541de40ecb8a00d4f02d50f5cb6
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-09-20 14:47:30 +0200

    feat: :sparkles: implementing functionality on the login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import { loginUser } from "../services/userService";
 import { FaRegUserCircle } from "react-icons/fa";
 
 function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    try {
+      const formData = new FormData();
+      formData.append("email", email);
+      formData.append("password", password);
+
+      const result = await loginUser(formData);
+      console.log(result);
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.error("Error loggin in user.");
+    }
+  }
+
   return (
     <div>
       <div className="container mx-auto p-4 flex justify-center">
+        <form onSubmit={handleSubmit}>
         <div className="grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex flex-col bg-white rounded-lg border-2 py-16 px-20">
             <div className="border-2 px-3 py-2 mb-4 rounded-xl self-start">
@@ -16,11 +38,15 @@ function Login() {
             <div className="flex flex-col gap-5">
               <div className="flex flex-col gap-3">
                 <label>Email</label>
-                <input className="border-2 px-2 py-1" placeholder="Email address"/>
+                <input className="border-2 px-2 py-1" placeholder="Email address"
+                onChange={(event) => {setEmail(event.target.value)}}
+                value={email}/>
               </div>
               <div className="flex flex-col gap-3">
                 <label>Password</label>
-                <input className="border-2 px-2 py-1" placeholder="Enter your password"/>
+                <input className="border-2 px-2 py-1" placeholder="Enter your password"
+                onChange={(event) => {setPassword(event.target.value)}}
+                value={password}/>
               </div>
               <div className="flex justify-between">
                 <div className="flex gap-2">
@@ -32,12 +58,13 @@ function Login() {
               <button className="bg-blue-500 text-white font-bold rounded py-1">Login</button>
               <div className="flex justify-center">
               <p className="text-gray-500">Don't have an account?
-              <button className="text-black underline ml-2">Sign up today?</button>
+              <button className="text-black underline ml-2" type="submit">Sign up today?</button>
               </p>
               </div>
             </div>
           </div>
         </div>
+        </form>
       </div>
     </div>
   )
