9dc4b744e43a978aecb949cbdbbb9d5a886c992f
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-09-20 14:24:18 +0200

    style: :lipstick: creating form content for free jobs and paid jobs

--- src/components/PostJobForm.tsx
+++ src/components/PostJobForm.tsx
@@ -45,23 +45,81 @@ function PostJobForm() {
             {activeTab === "button1" ? (
               <div className="flex flex-col gap-3 border-2 rounded-lg px-10 py-8">
                 <p className="font-bold text-lg">Job information</p>
-                <div>
+                <div className="flex flex-col gap-3">
                   <label>Job title</label>
-                  <input placeholder="Enter job title"/>
+                  <input className="border-2 px-3 rounded" placeholder="Enter job title"/>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <label>Job description</label>
+                  <textarea className="border-2 rounded px-3" placeholder="Enter a job description" />
                 </div>
                 <div className="flex justify-between gap-10">
                   <div className="flex flex-col gap-3">
-                    <label>Job department</label>
-                    <input className="border-2 rounded px-3" placeholder="Finance"/>
+                    <label>Job salary</label>
+                    <input className="border-2 rounded px-3" placeholder="Annual Salary in ZAR" />
                   </div>
                   <div className="flex flex-col gap-3">
-                    <label>Job type</label>
-                    <input className="border-2 rounded px-3" placeholder="Full time"/>
+                    <label>Job location</label>
+                    <input className="border-2 rounded px-3" placeholder="Gauteng"/>
+                  </div>
+                  
+                  </div>
+                  <div className="flex justify-between gap-10">
+                  <div className="flex flex-col gap-3">
+                    <label>Job level</label>
+                    <input className="border-2 rounded px-3" placeholder="Senior"/>
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <label>Department</label>
+                    <input className="border-2 rounded px-3" placeholder="Technology"/>
+                  </div>
+                  </div>
+                  <button className="bg-blue-500 text-white font-bold rounded py-1 my-3">Submit</button>
+                </div>
+            ) : (
+              <div className="flex flex-col gap-3 border-2 rounded-lg px-10 py-8">
+                <p className="font-bold text-lg">Job information</p>
+                <div className="flex flex-col gap-3">
+                  <label>Job title</label>
+                  <input className="border-2 px-3 rounded" placeholder="Enter job title"/>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <label>Job description</label>
+                  <textarea className="border-2 rounded px-3" placeholder="Enter a job description" />
+                </div>
+                <div className="flex justify-between gap-10">
+                  <div className="flex flex-col gap-3">
+                    <label>Job salary</label>
+                    <input className="border-2 rounded px-3" placeholder="Annual Salary in ZAR" />
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <label>Job location</label>
+                    <input className="border-2 rounded px-3" placeholder="Gauteng"/>
+                  </div>
+                  
+                  </div>
+                  <div className="flex justify-between gap-10">
+                  <div className="flex flex-col gap-3">
+                    <label>Job level</label>
+                    <input className="border-2 rounded px-3" placeholder="Senior"/>
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <label>Department</label>
+                    <input className="border-2 rounded px-3" placeholder="Technology"/>
+                  </div>
+                  </div>
+                  <button className="bg-blue-500 text-white font-bold rounded py-1 my-3">Submit</button>
+                  <div className="flex flex-col gap-4">
+                    <p className="font-bold">Featured job</p>
+                    <div className="flex flex-col items-center bg-gray-100 px-5 py-5 rounded-lg border-2">
+                      <p className="text-gray-500">If you already purchased a featured job credit, please insert your order ID <br /> below. If you didn't, you can go to <span className="text-blue-500">premium jobs page</span> to purchase it./</p>
+                      <div className="flex flex-col w-full gap-3 items-start">
+                      <label className="font-bold">Order ID</label>
+                      <input className="border-2 px-3 w-full rounded" placeholder="Insert your order ID" />
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ) : (
-              <p>Content for Button 2</p>
             )}
           </div>
         </div>
