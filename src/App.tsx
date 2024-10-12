9b43cfb8a6cca6fac3ac67deaad5fe09d4bc724f
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-12 20:26:13 +0200

    style: :lipstick: putting the navbar in its own componet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { useNavigate } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import Home from "../src/components/Home";
 import About from "../src/components/About";
 import Contact from "../src/components/Contact";
@@ -14,56 +14,11 @@ import PostJobForm from "./components/PostJobForm";
 import Profile from "./components/Profile";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // let navigate = useNavigate();
-  const login = () => {
-    setIsAuthenticated(true);
-    // navigate("/login");
-  }
-  const logout = () => {
-    setIsAuthenticated(false);
-  }
+  
 
   return (
     <div className="">
-      <nav className="flex h-20 items-center border-b-2">
-        <div className="items-start ml-20">
-          <a href="/" className="flex items-center font-semibold text-xl"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mx-1.5 text-sky-500">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
-</svg>
-SkillSpace</a>
-        </div>
-        <div className="ml-auto">
-          <ul className="flex gap-10 mr-20">
-            <li className="hover:text-sky-500">
-              <a href="/">Home</a>
-            </li>
-            <li className="hover:text-sky-500">
-              <a href="/about">About</a>
-            </li>
-            <li className="hover:text-sky-500">
-              <a href="/blog">Blog</a>
-            </li>
-            <li className="hover:text-sky-500">
-              <a href="/contact">Contact</a>
-            </li>
-            <li>
-              {isAuthenticated ? (
-                <button onClick={logout} className="flex items-center"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5 mx-1">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-              </svg>
-              Logout</button>
-              ) : (
-                  <button onClick={login} className="flex items-center"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5 mx-1">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-              </svg>
-              Login</button>
-              )}
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <NavBar />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
