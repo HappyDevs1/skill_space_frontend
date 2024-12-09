698263bab793a01398ee14119e96020ee4913bf4
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-09 13:19:22 +0200

    feat: creating the component to display all the companies

---
+++ src/components/Companies.tsx
@@ -0,0 +1,18 @@
+import Postjob from "./Postjob";
+
+function Companies () {
+  return (
+    <div>
+      <div className="flex justify-center">
+        <div className="mt-20 flex flex-col items-center">
+        <p className="font-bold text-4xl">Companies</p>
+        <p className="text-gray-500 mt-6">This is all the companies I will come back later to edit this text so that it makes sense. <br/> This is the companies that we are working with. I will come back later to edit this text.</p>
+        </div>
+        
+      </div>
+      <Postjob />
+    </div>
+  )
+}
+
+export default Companies;
