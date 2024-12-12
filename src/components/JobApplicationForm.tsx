a607960a989b48d799d49d2cf6858a3d85a3fa04
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-12 02:42:03 +0200

    feat: :sparkles: setting up a foundation to fetch viewed job to be applied for

--- src/components/JobApplicationForm.tsx
+++ src/components/JobApplicationForm.tsx
@@ -1,4 +1,27 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+interface Job {
+  title: string,
+  description: string,
+  price: string,
+  location: string,
+  level: string,
+  department: string,
+}
 function JobApplicationForm () {
+  const [job, setJobs] = useState<Job | null>(null);
+
+  const handleFetchJob = async () => {
+    try {
+      
+    } catch (error) {
+      console.error("Failed to fetch viewed job")
+    }
+  }
+  useEffect(() => {
+    
+  })
   return (
     <div>
       Job Application form
