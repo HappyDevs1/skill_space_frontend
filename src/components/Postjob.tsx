98dc0e48309ccab73f1376955a0728dfda5c8309
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-15 01:38:57 +0200

    style: :lipstick: making post job cards to be responsive

--- src/components/Postjob.tsx
+++ src/components/Postjob.tsx
@@ -1,30 +1,40 @@
 import { useNavigate } from "react-router-dom";
 
-function Postjob () {
+function Postjob() {
   const navigate = useNavigate();
+  
   const handlePostJob = async () => {
     try {
-      navigate("/create/job")
+      navigate("/create/job");
     } catch (error) {
-      console.error("Failed to redirect to post job form")
+      console.error("Failed to redirect to post job form");
     }
-  }
+  };
+
   return (
     <div>
-      <div className="flex mt-24 justify-evenly mb-20">
-        <div className="bg-blue-600 px-16 py-10 rounded">
+      <div className="flex flex-col sm:flex-row mt-24 justify-evenly mb-20">
+        <div className="bg-blue-600 px-16 py-10 rounded mb-4 sm:mb-0">
           <p className="text-white text-2xl mb-3 font-bold">Post a featured job</p>
-          <p className="text-white mb-8">You can post a featured job, I will come back <br /> and edit this text to ensure that it makes sense.</p>
-          <button className="bg-white rounded px-2 py-1" onClick={handlePostJob}>Post a featured job</button>
+          <p className="text-white mb-8">
+            You can post a featured job, I will come back <br /> and edit this text to ensure that it makes sense.
+          </p>
+          <button className="bg-white rounded px-2 py-1" onClick={handlePostJob}>
+            Post a featured job
+          </button>
         </div>
         <div className="bg-gray-100 px-16 py-10 rounded">
           <p className="text-2xl font-bold mb-3">Post a free job</p>
-          <p className="text-gray-600 mb-8">You can post a free job, I will come back <br /> and edit this text to ensure that it makes sense.</p>
-          <button className="bg-blue-500 text-white rounded px-2 py-1" onClick={handlePostJob}>Post a free job</button>
+          <p className="text-gray-600 mb-8">
+            You can post a free job, I will come back <br /> and edit this text to ensure that it makes sense.
+          </p>
+          <button className="bg-blue-500 text-white rounded px-2 py-1" onClick={handlePostJob}>
+            Post a free job
+          </button>
         </div>
       </div>
     </div>
-  )
-};
+  );
+}
 
 export default Postjob;
