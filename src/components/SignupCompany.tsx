3b439f3901cf1fa754d78d16beec84692f01647a
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-15 15:14:59 +0200

    style: :lipstick: making the sign up company component to be as identical as the signup user component

--- src/components/SignupCompany.tsx
+++ src/components/SignupCompany.tsx
@@ -44,33 +44,34 @@ function SignUpCommpany () {
   
   const handleUserSignup = async () => {
     try {
-      navigate("/user/signup");
+      navigate("/user/create");
     } catch (error) {
       console.log("Error redirecting to the user signup page", error);
     }
   }
   return (
 <div>
-      {
-        loading ? (
-          <div>
-            <CircularIndeterminate />
-          </div>
-        ) : (
-          // <form onSubmit={handleSubmit}>
-        <div className="flex mt-24 justify-center mb-20">
-          <div className="px-16 py-10 rounded border-2">
-            <p className="text-3xl mb-3 font-bold">Create account</p>
+  {loading ? (
+    <div>
+      <CircularIndeterminate />
+    </div>
+  ) : (
+    <form onSubmit={handleSubmit}>
+      <div className="flex mt-24 justify-center mb-20">
+        <div className="w-full max-w-screen-xl grid grid-cols-1 lg:grid-cols-2 gap-8">
+          {/* Form Container */}
+          <div className="px-4 py-8 sm:px-10 sm:py-12 md:px-16 md:py-10 rounded border-2">
+            <p className="text-2xl sm:text-3xl mb-3 font-bold">Create account</p>
             <div className="flex items-center gap-2">
               <p className="text-gray-500">Already have an account?</p>
-              {/* <button className="underline" onClick={redirectToLogin}>Sign in.</button> */}
+              <button className="underline" onClick={() => {navigate("/company/login")}}>Sign in.</button>
             </div>
             <div className="flex flex-col gap-3">
-              <div className="flex gap-5 mt-5">
-                <div className="flex flex-col gap-3">
+              <div className="flex flex-col md:flex-row gap-5 mt-5">
+                <div className="flex flex-col gap-3 w-full md:w-1/2">
                   <label>Name</label>
                   <input
-                    className="border-2 rounded px-3 py-1"
+                    className="border-2 rounded px-3 py-1 w-full"
                     placeholder="John Carter"
                     value={name}
                     onChange={(event) => {
@@ -79,10 +80,10 @@ function SignUpCommpany () {
                     required
                   />
                 </div>
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-3 w-full md:w-1/2">
                   <label>Email</label>
                   <input
-                    className="border-2 rounded px-3 py-1"
+                    className="border-2 rounded px-3 py-1 w-full"
                     placeholder="Email address"
                     value={email}
                     onChange={(event) => setEmail(event.target.value)}
@@ -90,10 +91,10 @@ function SignUpCommpany () {
                   />
                 </div>
               </div>
-              <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-3 mt-5">
                 <label>Password</label>
                 <input
-                  className="border-2 rounded px-3 py-1"
+                  className="border-2 rounded px-3 py-1 w-full"
                   placeholder="Enter your password"
                   type="password"
                   value={password}
@@ -101,24 +102,14 @@ function SignUpCommpany () {
                   required
                 />
               </div>
-              <div className="flex flex-col gap-3">
-                <label>
-                  About the company
-                </label>
-                <textarea className="border-2 rounded px-1 py-1 h-20"
-                placeholder="Information about the company..."
-                value={about}
-                onChange={(event) => setAbout(event.target.value)}
-                required
+              <div className="flex flex-col gap-3 mt-5">
+                <label>About compamy</label>
+                <textarea className="border-2 rounded px-3 py-1 h-20 w-full"
+                  placeholder="Information about the company"
+                  value={about}
+                  onChange={(event) => setAbout(event.target.value)}
+                  required
                 />
-                {/* <select
-                  className="border-2 rounded px-3 py-1"
-                  value={role}
-                  // onChange={(event) => setRole(event.target.value)}
-                >
-                  <option value={freelancer}>I want a job</option>
-                  <option value={client}>I want to hire</option>
-                </select> */}
               </div>
               {/* <div>
                 <label>Profile Picture</label>
@@ -134,35 +125,31 @@ function SignUpCommpany () {
                 <input type="checkbox" className="self-start my-2" />
                 <div className="flex flex-col">
                   <p className="text-gray-500">
-                    I have read accepted the SkillSpace App Ltd. <br />
-                    Terms & Conditions and Private Policy.
+                    I have read and accepted the SkillSpace App Ltd. <br />
+                    Terms & Conditions and Privacy Policy.
                   </p>
                 </div>
               </div>
-              <button className="bg-blue-600 text-white font-bold rounded py-1"
-              type="submit"
-              onClick={handleSubmit}
-              >
+              <button className="bg-blue-600 text-white font-bold rounded py-2 w-full sm:w-auto mt-5" type="submit">
                 Sign up
               </button>
               <button 
-              onClick={handleUserSignup}
-              className="underline">
-                Signup as an individual user?
-              </button>
+                className="text-green underline mt-3 w-full sm:w-auto"
+                onClick={handleUserSignup}>Signup as a company?</button>
             </div>
           </div>
-          <div className="flex flex-col items-center px-16 py-10 rounded">
+
+          {/* Our Numbers Container */}
+          <div className="flex flex-col items-center px-4 py-8 sm:px-16 sm:py-10 rounded">
             <div className="flex flex-col items-center gap-3">
-              <p className="font-bold text-4xl">Our numbers</p>
-              <p className="text-gray-500">
+              <p className="font-bold text-3xl sm:text-4xl">Our numbers</p>
+              <p className="text-gray-500 text-sm sm:text-base">
                 This is a test message I will come back to it later on to fix it
-                so <br /> that it makes sense. I will come back to it later on
-                to finalise it.
+                so that it makes sense. I will come back to it later on to finalize it.
               </p>
             </div>
-            <div className="container m-auto">
-              <div className="flex grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 mx-auto justify-items-center">
+            <div className="container m-auto mt-8">
+              <div className="flex grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 mx-auto justify-items-center">
                 <div className="flex justify-center gap-7 py-5 px-3 w-full max-w-xs rounded-lg border-2">
                   <div className="flex flex-col items-center">
                     <p className="text-4xl font-bold">
@@ -207,10 +194,10 @@ function SignUpCommpany () {
             </div>
           </div>
         </div>
-      // </form>
-        )
-      }
-    </div>
+      </div>
+    </form>
+  )}
+</div>
   )
 }
 
