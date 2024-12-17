5fe19d7051189e6b6ae2c90e1d7432c4bc0d22cd
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-17 09:51:56 +0200

    docs: :memo: adding a blueprint of how to implement the company jobs and applicants of a job feature

---
+++ src/components/CompanyJobs.tsx
@@ -0,0 +1,18 @@
+function CompanyJobs () {
+  return (
+    <div>
+      {/* This is the component I will use to display the jobs of a company
+      Function 1
+      1. Get both Company id and get all jobs by id
+      2. If an id match then return the job
+      3. Display the job
+
+      function 2
+      1. Get the job id then get all application by id
+      2.If the id match then return the application details
+      3. Display all the applications */}
+    </div>
+  )
+}
+
+export default CompanyJobs;
