2a726b2a9f82d38b12a3962d9d689a1eefd47a71
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-18 13:49:37 +0200

    feat: :sparkles: displaying the job openings of the company

--- src/components/CompanyJobs.tsx
+++ src/components/CompanyJobs.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
 import { getAllJobs } from "../services/jobService";
+import { CiLocationOn } from "react-icons/ci";
+import { VscGraph } from "react-icons/vsc";
+import { IoBriefcaseOutline } from "react-icons/io5";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 interface Company {
   name: string,
@@ -46,6 +50,14 @@ function CompanyJobs () {
       console.error("Failed to fetch jobs under this company: ", error);
     }
   };
+
+  const handleJobClick = async () => {
+    try {
+
+    } catch (error) {
+      console.error("Failed to view job applicants");
+    }
+  }
   
 
   useEffect(() => {
@@ -64,7 +76,64 @@ function CompanyJobs () {
       1. Get the job id then get all application by id
       2.If the id match then return the application details
       3. Display all the applications */}
-      
+                  <div className="flex flex-col items-center justify-center w-full flex-grow mt-28 px-4">
+              <p className="font-semibold text-xl mb-6 text-center">
+                Your Job Openings
+              </p>
+              {companyJobs.length > 0 ? (
+                companyJobs.map((job) => (
+                  <div
+                    key={job._id}
+                    className="flex flex-col md:flex-row border rounded border-gray-400 px-3 py-3 w-full md:w-auto cursor-pointer mb-4"
+                    onClick={() => handleJobClick(job)}
+                  >
+                    {/* Left Section: Image */}
+                    <div className="flex items-center justify-center bg-sky-100 px-6 py-8 rounded-md md:px-11 md:py-14">
+                      <img
+                        src={job.company.profilePicture}
+                        className="w-16 h-16 md:w-13 md:h-11 object-cover rounded"
+                        alt={job.company.name}
+                      />
+                    </div>
+
+                    {/* Middle Section: Job Info */}
+                    <div className="mt-4 md:mt-0 md:pl-7 md:pr-20">
+                      <p className="font-semibold text-gray-400 my-2 text-center md:text-left">
+                        {job.company.name}
+                      </p>
+                      <p className="font-bold text-xl text-center md:text-left">
+                        {job.title}
+                      </p>
+                      <div className="flex flex-wrap gap-2.5 mt-4 justify-center md:justify-start">
+                        <div className="flex items-center border border-gray-300 rounded p-1">
+                          <CiLocationOn className="text-blue-500 mr-1.5 h-3 w-3" />
+                          <p className="text-xs font-medium">{job.location}</p>
+                        </div>
+                        <div className="flex items-center border border-gray-300 rounded p-1">
+                          <VscGraph className="text-blue-500 mr-1.5 h-3 w-3" />
+                          <p className="text-xs font-medium">{job.level}</p>
+                        </div>
+                        <div className="flex items-center border border-gray-300 rounded p-1">
+                          <IoBriefcaseOutline className="text-blue-500 mr-1.5 h-3 w-3" />
+                          <p className="text-xs font-medium">
+                            {job.department}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Right Section: Arrow */}
+                    <div className="mt-4 md:mt-0 flex items-center justify-center">
+                      <div className="border border-gray-400 rounded p-1">
+                        <IoIosArrowRoundForward className="h-7 w-7 text-gray-500" />
+                      </div>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <div>No jobs available</div>
+              )}
+            </div>
     </div>
   )
 }
