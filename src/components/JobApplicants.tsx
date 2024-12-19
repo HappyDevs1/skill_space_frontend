5652c61e97b357a9e91d569b4b8d93f170dacc48
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-19 12:04:50 +0200

    feat: :sparkles: displaying the applicants of a job in a table format

--- src/components/JobApplicants.tsx
+++ src/components/JobApplicants.tsx
@@ -41,6 +41,7 @@ function JobApplicants () {
 
       console.log("Filtered applicants for job: ", filteredApplicants);
       setApplicants(filteredApplicants);
+
     } catch (error) {
       console.error("Failed to fetch applicants for the job: ", error);
     }
@@ -50,8 +51,45 @@ function JobApplicants () {
     handleFetchApplicants();
   }, [id]);
   return (
-    <div>
+    <div className="container mx-auto px-4">
+      <h1 className="text-2xl font-bold mb-4">Job applicants</h1>
 
+      {/* Table */}
+      <div className="overflow-x-auto">
+        <table className="min-w-full border-collapse border border-gray-300">
+          <thead className="bg-indigo-50">
+            <tr>
+              <th className="border border-gray-300 p-2 text-left">Name</th>
+              <th className="border border-gray-300 p-2 text-left">Email Address</th>
+              <th className="border border-gray-300 p-2 text-left">Phone number</th>
+              <th className="border border-gray-300 p-2 text-left">About</th>
+              <th className="border border-gray-300 p-2 text-left">Portfolio</th>
+              <th className="border border-gray-300 p-2 text-left">CV</th>
+              <th className="border border-gray-300 p-2 text-left">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {applicants.map((applicant) => (
+              <tr key={applicant._id} className="odd:bg-white even:bg-gray-50">
+                <td className="border border-gray-300 p-2">{applicant.name}</td>
+                <td className="border border-gray-300 p-2">{applicant.email}</td>
+                <td className="border border-gray-300 p-2">{applicant.phone}</td>
+                <td className="border border-gray-300 p-2">{applicant.about}</td>
+                <td className="border border-gray-300 p-2">{applicant.portfolio}</td>
+                <td className="border border-gray-300 p-2">{applicant.cv}</td>
+                <td className="border border-gray-300 p-2">{applicant.status}</td>
+              </tr>
+            ))}
+            {applicants.length === 0 && (
+              <tr>
+                <td colSpan={3} className="text-center p-4">
+                  This job has no applicants
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
